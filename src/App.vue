b33490a9614d283de714ee3d768e6f34e3130ae6
Wallace-A <andrew.wallace.english@gmail.com>
2021-09-30 13:28:27 +0900

    app styling

--- src/App.vue
+++ src/App.vue
@@ -23,8 +23,28 @@
     }
   }
   
-
   body {
     background-color: #35495E;
+  }
+  a {
+    text-decoration: none;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 16px;
+    background-color: #2c3D4E;
+    box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+  }
+
+  h1 {
+    color: white;
+    font-size: 28px;
+
+    span {
+      color: #42B883;
+    }
   }
 </style>
