337aaaa1962cb7ae15f5352a56f89b2ab0fa991e
Wallace-A <andrew.wallace.english@gmail.com>
2021-09-30 13:45:22 +0900

    added detail route

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="home">
+    <div class="feature-card">
+      <router-link to="/movie/tt0083658">
+        <img src="https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg" alt="Naruto Poster" class="featured-img" />
+        <div class="detail">
+          <h3>Blade Runner</h3>
+          <p>Movie description</p>
+        </div>
+      </router-link>
+    </div>
     HOME
   </div>
 </template>
@@ -10,3 +19,39 @@
 export default {
 }
 </script>
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: white;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: white;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="movie-detail">
+      DETAIL
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
