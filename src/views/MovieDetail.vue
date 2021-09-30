20b81805b0e695ba1847b0b3ba12959a3c2a3008
Wallace-A <andrew.wallace.english@gmail.com>
2021-09-30 15:58:12 +0900

    movie detail api request

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="movie-detail">
-      DETAIL {{ $route.params.id }}
+      <h2>MOVIE TITLE</h2>
   </div>
 </template>
 
 <script>
-export default {
+import {ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "@/env.js";
 
+export default {
+    setup () {
+        const movie = ref({});
+        //can't use $route to get id, need to use useRoute instead
+        const route = useRoute();
+
+        onBeforeMount(() => {
+            fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+        });
+
+        return{
+            movie
+        }
+    }
 }
 </script>
 
